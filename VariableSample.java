a1e277227da09353ed3b423f8a48564aeb3e0c2e
M.REZA ATHAULLAH 2201082032 <126840109+Rezaathaullah@users.noreply.github.com>
2023-03-06 13:31:32 +0700

    VariableSamples

---
+++ VariableSample.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package reza030323;
+
+/**
+ *
+ * @author Rafki
+ */
+public class VariableSample {
+     public static void main(String[] args) {
+        //deklarasi a tipe data dengan nama variable
+        //result dan tipe data boolean
+        boolean result;
+        
+        //deklarasi tipe data dengan nama variable
+        //grade, double tipe data dan telah di inisialisasi
+        //to 0.0
+        double grade = 0.0;
+    }
+}
