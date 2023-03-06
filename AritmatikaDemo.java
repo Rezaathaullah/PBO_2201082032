955c64b6e1ee640a5bb1652092ca23653205bfa2
M.REZA ATHAULLAH 2201082032 <126840109+Rezaathaullah@users.noreply.github.com>
2023-03-06 13:26:09 +0700

    memasukkan file aritmatika

---
+++ AritmatikaDemo.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package reza030323;
+
+/**
+ *
+ * @author Rafki
+ */
+public class AritmatikaDemo {
+    public static void main (String [] args){
+    
+        int i = 37;
+        int j = 42;
+        double x = 27.475;
+        double y = 7.22;
+        System.out.println("Variable values...");
+        System.out.println(" i = " + i);
+        System.out.println(" j = " + j);
+        System.out.println(" x = " + x );
+        
+            System.out.println(" y =  " + y  );
+         // penjumlahan angka   
+        System.out.println(" adding... "  );
+        System.out.println(" i + j = " + (i + j));
+        System.out.println(" x + j = " + (x + j));
+        
+        // pengurangan angka
+        System.out.println(" subtracting... "  );
+        System.out.println(" i - j = " + (i - j));
+        System.out.println(" x - j = " + (x - j));
+        
+        System.out.println(" multiplying... "  );
+        System.out.println(" i * j = " + (i * j));
+        System.out.println(" x * j = " + (x * j));
+        
+        System.out.println(" Dividing... "  );
+        System.out.println(" i / j = " + (i / j));
+        System.out.println(" x / j = " + (x / j));
+        
+        
+             System.out.println("Computing the reminder");
+        System.out.println(" i % j = " + (i % j));
+        System.out.println(" x % j = " + (x % j));
+        
+        //gabungan angka
+         System.out.println("Mixing tipes....");
+         
+        System.out.println(" i + j = " + (i + j));
+        System.out.println(" x * j = " + (x * j));
+        
+        
+    }
+}
+
